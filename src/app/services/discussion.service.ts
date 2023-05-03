1a9759d2199896b6a70a2331a45c17b30285670d
mahmoudtayem <tayemmahmoud87@gmail.com>
2023-05-03 12:10:52 +0400

    adding content polls adding css classes

---
+++ src/app/services/discussion.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+declare const Liferay: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiscussionService {
+
+  constructor() { }
+}
